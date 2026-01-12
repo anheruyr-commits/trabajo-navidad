d2ec58265dc3bc29fbdc96af2b272fb2af8bc97c
anheruyr-commits <anheruyr@alumnos.alborfp.com>
2026-01-12 14:16:26 +0100

    com,i

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-import{} from "./helpers/"
+// Esperamos a que el DOM cargue
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const toggleButton = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector(".nav-menu");
+
+    toggleButton.addEventListener("click", () => {
+        // Alternar la clase que muestra el menú
+        navMenu.classList.toggle("nav-menu_visible");
+    });
+});
